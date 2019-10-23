ddcc08b6451fd04cdecf35eafd53143dea5bf62d
kezyagth <kezyagatha@gmail.com>
2019-10-23 17:15:34 +0700

    myproject

---
+++ index.js
@@ -0,0 +1,92 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const Joi = require('@hapi/joi');
+const server = new Hapi.Server({
+    host: 'localhost',
+    port: 3101
+});
+server.route([
+    {
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'I am Root Route'
+        }
+    },
+    {
+        method: 'POST',
+        path: '/persegi',
+        config: {
+            validate: {
+                payload:{
+                    panjang: Joi.number().min(1).required(),
+                    lebar: Joi.number().min(1).required()
+                }
+            }
+        },
+        handler: (request, h) => {
+            console.log(request.payload);
+            let panjangRequest= request.payload.panjang;
+            let lebarRequest = request.payload.lebar;
+            let hasil = parseInt(panjangRequest) * parseInt(lebarRequest)
+            const data = {data: 'hello detail users',...request.payload,hasilPerhitungan: hasil}   //bkin respon berbentuk json     
+                return h.response(data).code(200) //return output berupa json
+        }
+    },
+    {
+        method: 'POST',
+        path: '/ganjilgenap',
+        config: {
+            validate: {
+                payload:{
+                    angka: Joi.number().min(1).required()
+                    
+                }
+            }
+        },
+        handler: (request, h) => {
+            console.log(request.payload);
+            let angka = request.payload.angka;
+            
+            if(angka %2 == 0){
+                const data = {data: 'hello detail users',...request.payload,hasilPerhitungan: 'genap'}   //bkin respon berbentuk json     
+                return h.response(data).code(200)
+            }else{
+                const data = {data: 'hello detail users',...request.payload,hasilPerhitungan: 'ganjil'}   //bkin respon berbentuk json     
+                return h.response(data).code(200)
+            }
+        }
+    },
+
+    {
+        method: 'GET',
+        path: '/hello',
+        handler: (request, h) => {
+            return {msg: 'I am Hello'};
+        }
+    },
+    {
+        method: 'POST',
+        path: '/register',
+        handler: (request, h) => {
+            return {msg: 'i am register using post'};
+        }
+    },
+]);
+const main = async () => {
+    await server.register(require('./src/routes/users'));
+    await server.start()
+    return server
+};
+
+
+
+main().then(server => {
+    console.log('Server Running at:',
+    server.info.uri)
+}).catch(err =>{
+    console.log(err)
+    process.exit(1)
+})
+
---
+++ src/routes/users.js
@@ -0,0 +1,39 @@
+'use strict';
+const Joi = require('@hapi/joi');
+
+
+exports.plugin = {
+    pkg: require('../../package.json'),
+    name: 'route-books',
+    register: async (server, options,next) => {
+        const basePath = '/api/v1/'; //
+        server.route([
+            {
+                method: 'GET',
+                path: basePath + 'users',
+                handler: (request, h) => {
+                    const data = {data: 'Hello from Users'}
+                    return h.response(data).code(200)
+                }
+            },
+            {
+                method: 'GET',
+                path: basePath + 'users/{id}',
+                options:{
+                    validate:{
+                        params:{
+                            id : Joi.number().required().min(1)
+                        },
+                        query: {
+                            page: Joi.number().min(0).default(1)
+                        }
+                    }
+                },
+                handler: (request, h) => {
+                    return 'Hello from User' + request.params.id
+                    + 'Parameter Page =' + request.query.page;
+                }
+            }
+        ]);
+    }
+};
